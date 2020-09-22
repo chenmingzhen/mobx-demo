0a452fef894674f17efd8ab3afd4038ca73ac51a
chenmingzhen <214164051@qq.com>
2020-09-22 08:42:36 +0800

    first commit

--- src/hook/useCount.js
+++
@@ -1,9 +0,0 @@
-import {useState, useEffect} from 'react';
-
-export default function useCount(initCount) {
-    const [count, setCount] = useState(initCount);
-    useEffect(() => {
-        console.log('count change');
-    }, [count]);
-    return count;
-}
